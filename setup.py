6cedfc330b94f35ffa8c114c19f397c7a5d74de8
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-10-22 16:12:28 -0400

    add hints package to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import fnmatch
+import os
+
+
+def is_package(path):
+    is_svn_dir = fnmatch.fnmatch(path, '*/.svn*')
+    is_test_module = fnmatch.fnmatch(path, '*tests')
+    return not (is_svn_dir or is_test_module)
+
+packages = [p for p, _, _ in os.walk('xivo_confgen') if is_package(p)]
+
 
 setup(
     name='xivo-confgen',
@@ -11,8 +22,7 @@ setup(
     author_email='xivo-dev@lists.proformatique.com',
     url='http://wiki.xivo.io/',
     license='GPLv3',
-    packages=['xivo_confgen',
-              'xivo_confgen.generators'],
+    packages=packages,
     scripts=['bin/xivo-confgen', 'bin/xivo-confgend'],
     data_files=[('/etc/xivo', ['etc/xivo-confgen.conf', 'etc/xivo-confgend.conf']),
                 ('/etc/xivo/xivo-confgend/asterisk', ['etc/asterisk/contexts.conf'])],

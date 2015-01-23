1602e29d3f3579505d7dc28411e8b29b58f95086
Sebastien Duthil <sduthil@avencall.com>
2015-01-23 14:10:39 -0500

    Revert "Merge branch 'find_packages'"
    This reverts commit c78f55b1e5be2bd3f4020aa97b8b7455e93c7826, reversing
    changes made to 6cedfc330b94f35ffa8c114c19f397c7a5d74de8.

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
-from setuptools import find_packages
+from distutils.core import setup
 import fnmatch
 import os
+
+
+def is_package(path):
+    is_svn_dir = fnmatch.fnmatch(path, '*/.svn*')
+    is_test_module = fnmatch.fnmatch(path, '*tests')
+    return not (is_svn_dir or is_test_module)
+
+packages = [p for p, _, _ in os.walk('xivo_confgen') if is_package(p)]
 
 
 setup(
@@ -15,7 +22,7 @@ setup(
     author_email='xivo-dev@lists.proformatique.com',
     url='http://wiki.xivo.io/',
     license='GPLv3',
-    packages=find_packages(),
+    packages=packages,
     scripts=['bin/xivo-confgen', 'bin/xivo-confgend'],
     data_files=[('/etc/xivo', ['etc/xivo-confgen.conf', 'etc/xivo-confgend.conf']),
                 ('/etc/xivo/xivo-confgend/asterisk', ['etc/asterisk/contexts.conf'])],

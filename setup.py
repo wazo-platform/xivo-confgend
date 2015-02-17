c12ffe26cf061925c17b71470ed653c683dd6dae
Sebastien Duthil <sduthil@avencall.com>
2015-02-17 09:28:53 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -15,15 +15,15 @@ packages = [p for p, _, _ in os.walk('xivo_confgen') if is_package(p)]
 
 
 setup(
-    name='xivo-confgen',
+    name='xivo-confgend',
     version='0.1',
     description='XIVO Configurations Generator',
     author='Avencall',
     author_email='xivo-dev@lists.proformatique.com',
-    url='http://wiki.xivo.io/',
+    url='http://www.xivo.io/',
     license='GPLv3',
     packages=packages,
-    scripts=['bin/xivo-confgen', 'bin/xivo-confgend'],
-    data_files=[('/etc/xivo', ['etc/xivo-confgen.conf', 'etc/xivo-confgend.conf']),
+    scripts=['bin/xivo-confgend'],
+    data_files=[('/etc/xivo', ['etc/xivo-confgend.conf']),
                 ('/etc/xivo/xivo-confgend/asterisk', ['etc/asterisk/contexts.conf'])],
 )

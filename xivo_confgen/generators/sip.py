005e5a4757bdc0821f7a47635a574f722c36c11f
Gregory Eric Sanderson <gsanderson@avencall.com>
2016-01-15 11:54:03 -0500

    replace old sip trunk generator with new one

--- xivo_confgen/generators/sip.py
+++ xivo_confgen/generators/sip.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2011-2015 Avencall
+# Copyright (C) 2011-2016 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+from xivo_dao.helpers.db_utils import session_scope
 from xivo_dao import asterisk_conf_dao
 
 CC_POLICY_ENABLED = 'generic'
@@ -27,6 +28,9 @@ CCNR_AVAILABLE_TIMER = 900
 
 class SipConf(object):
 
+    def __init__(self, trunk_generator):
+        self.trunk_generator = trunk_generator
+
     def generate(self, output):
         data_general = asterisk_conf_dao.find_sip_general_settings()
         self._gen_general(data_general, output)
@@ -36,9 +40,9 @@ class SipConf(object):
         self._gen_authentication(data_auth, output)
         print >> output
 
-        data_trunk = asterisk_conf_dao.find_sip_trunk_settings()
-        self._gen_trunk(data_trunk, output)
-        print >> output
+        with session_scope():
+            self._gen_trunk(output)
+            print >> output
 
         data_pickup = asterisk_conf_dao.find_sip_pickup_settings()
         data_user = asterisk_conf_dao.find_sip_user_settings()
@@ -74,23 +78,9 @@ class SipConf(object):
                 mode = '#' if auth['secretmode'] == 'md5' else ':'
                 print >> output, "auth = %s%s%s@%s" % (auth['user'], mode, auth['secret'], auth['realm'])
 
-    def _gen_trunk(self, data_trunk, output):
-        for trunk in data_trunk:
-            print >> output, "\n[%s]" % trunk['name']
-
-            for k, v in trunk.iteritems():
-                if k in ('id', 'name', 'protocol', 'category', 'commented', 'disallow') or v in (None, ''):
-                    continue
-
-                if isinstance(v, unicode):
-                    v = v.encode('utf8')
-
-                if k == 'allow':
-                    print >> output, "disallow = all"
-                    for c in v.split(','):
-                        print >> output, "allow = " + str(c)
-                else:
-                    print >> output, k, '=', v
+    def _gen_trunk(self, output):
+        for line in self.trunk_generator.generate():
+            print >> output, line
 
     def _gen_user(self, data_pickup, data_user, ccss_options, output):
         sip_unused_values = (

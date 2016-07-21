6c3a73488407f8d744a5378e744b8831b0fb0139
Sebastien Duthil <sduthil@proformatique.com>
2016-07-21 15:22:52 -0400

    refactor handler lookup

--- xivo_confgen/confgen.py
+++ xivo_confgen/confgen.py
@@ -16,15 +16,17 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import time
-import sys
-
-from stevedore import driver, exception
 
 from xivo_confgen import cache
 from xivo_confgen.asterisk import AsteriskFrontend
 from xivo_confgen.xivo import XivoFrontend
 from xivo_confgen.dird import DirdFrontend
 from xivo_confgen.dird_phoned import DirdPhonedFrontend
+from xivo_confgen.handler import CachedHandlerFactoryDecorator
+from xivo_confgen.handler import MultiHandlerFactory
+from xivo_confgen.handler import PluginHandlerFactory
+from xivo_confgen.handler import FrontendHandlerFactory
+from xivo_confgen.handler import NullHandlerFactory
 from xivo_dao.helpers.db_utils import session_scope
 from twisted.internet.protocol import Protocol, ServerFactory
 
@@ -55,19 +57,20 @@ class ConfgendFactory(ServerFactory):
     protocol = Confgen
 
     def __init__(self, cachedir, config):
-        self.frontends = {
+        frontends = {
             'asterisk': AsteriskFrontend(config),
             'dird': DirdFrontend(),
             'dird-phoned': DirdPhonedFrontend(),
             'xivo': XivoFrontend(),
         }
         self._cache = cache.FileCache(cachedir)
-        self._handlers = {}
-        self._config = config
+        self._handler_factory = CachedHandlerFactoryDecorator(MultiHandlerFactory([PluginHandlerFactory(config),
+                                                                                   FrontendHandlerFactory(frontends),
+                                                                                   NullHandlerFactory()]))
 
     def generate(self, resource, filename):
         cache_key = '{}/{}'.format(resource, filename)
-        handler = self._get_handler(resource, filename)
+        handler = self._handler_factory.get(resource, filename)
         with session_scope():
             content = handler() or self._get_cached_content(cache_key)
         return self._encode_and_cache(cache_key, content)
@@ -79,55 +82,6 @@ class ConfgendFactory(ServerFactory):
         except AttributeError:
             print "No cached content for {}".format(cache_key)
 
-    def _get_handler(self, resource, filename):
-        handler_key = '{}.{}'.format(resource, filename)
-        if handler_key not in self._handlers:
-            self._find_and_update_handler(handler_key, resource, filename)
-        return self._handlers[handler_key]
-
-    def _find_and_update_handler(self, handler_key, resource, filename):
-        plugin = self._find_plugin(resource, filename)
-        if plugin:
-            self._handlers[handler_key] = plugin.generate
-            return
-
-        frontend_callback = self._find_frontend_callback(resource, filename)
-        if frontend_callback:
-            self._handlers[handler_key] = frontend_callback
-            return
-
-        self._handlers[handler_key] = _NullHandler(resource, filename).generate
-
-    def _find_plugin(self, resource, filename):
-        suffix = '{}.{}'.format(resource, filename)
-        namespace = 'confgend.{}'.format(suffix)
-        driver_name = self._config['plugins'].get(suffix)
-        if not driver_name:
-            return None
-
-        try:
-            return driver.DriverManager(
-                namespace=namespace,
-                name=driver_name,
-                invoke_on_load=True,
-                invoke_args=(self._config,),
-            ).driver
-        except exception.NoMatches:
-            return
-
-    def _find_frontend_callback(self, frontend_name, filename):
-        callback = filename.replace('.', '_')
-        frontend = self.frontends.get(frontend_name)
-        if frontend is None:
-            return
-
-        try:
-            return getattr(frontend, callback)
-        except Exception as e:
-            import traceback
-            print e
-            traceback.print_exc(file=sys.stdout)
-
     def _encode_and_cache(self, cache_key, content):
         if not content:
             return
@@ -135,12 +89,3 @@ class ConfgendFactory(ServerFactory):
         encoded_content = content.encode('utf-8')
         self._cache.put(cache_key, encoded_content)
         return encoded_content
-
-
-class _NullHandler(object):
-
-    def __init__(self, resource, filename):
-        self._error_msg = 'No handler found for {}/{}'.format(resource, filename)
-
-    def generate(self):
-        print self._error_msg
---
+++ xivo_confgen/tests/test_handler.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from hamcrest import assert_that
+from hamcrest import equal_to
+from hamcrest import calling
+from hamcrest import none
+from hamcrest import raises
+from mock import Mock
+from mock import patch
+from mock import sentinel as s
+from unittest import TestCase
+
+from ..handler import CachedHandlerFactoryDecorator
+from ..handler import FrontendHandlerFactory
+from ..handler import MultiHandlerFactory
+from ..handler import NoSuchHandler
+from ..handler import NullHandlerFactory
+from ..handler import PluginHandlerFactory
+
+
+class TestCachedHandlerFactoryDecorator(TestCase):
+
+    def test_that_two_gets_give_the_same_result(self):
+        decorated = Mock()
+        decorated.get.side_effect = [1, 2]
+
+        factory = CachedHandlerFactoryDecorator(decorated)
+
+        result_1 = factory.get(s.resource, s.filename)
+        result_2 = factory.get(s.resource, s.filename)
+
+        assert_that(result_1, equal_to(result_2))
+
+    def test_given_decorated_raises_when_get_then_raise(self):
+        decorated = Mock()
+        decorated.get.side_effect = NoSuchHandler
+
+        factory = CachedHandlerFactoryDecorator(decorated)
+
+        assert_that(calling(factory.get).with_args(s.resource, s.filename),
+                    raises(NoSuchHandler))
+
+
+class TestMultiHandlerFactory(TestCase):
+
+    def test_given_no_fail_when_get_then_first_result_is_returned(self):
+        factory1 = Mock()
+        factory1.get.return_value = 1
+        factory2 = Mock()
+        factory2.get.return_value = 2
+        multi_factory = MultiHandlerFactory([factory1, factory2])
+
+        result = multi_factory.get(s.resource, s.filename)
+
+        assert_that(result, equal_to(1))
+
+    def test_given_first_factory_fails_when_get_then_second_result_is_returned(self):
+        factory1 = Mock()
+        factory1.get.side_effect = NoSuchHandler()
+        factory2 = Mock()
+        factory2.get.return_value = 2
+        multi_factory = MultiHandlerFactory([factory1, factory2])
+
+        result = multi_factory.get(s.resource, s.filename)
+
+        assert_that(result, equal_to(2))
+
+    def test_given_all_factories_fail_when_get_then_raise(self):
+        factory1 = Mock()
+        factory1.get.side_effect = NoSuchHandler()
+        factory2 = Mock()
+        factory2.get.side_effect = NoSuchHandler()
+        multi_factory = MultiHandlerFactory([factory1, factory2])
+
+        assert_that(calling(multi_factory.get).with_args(s.resource, s.filename),
+                    raises(NoSuchHandler))
+
+
+class TestPluginHandlerFactory(TestCase):
+
+    @patch('xivo_confgen.handler.driver')
+    def test_given_no_driver_found_when_get_then_raise(self, stevedore_driver):
+        stevedore_driver.DriverManager.side_effect = RuntimeError
+        resource = 'resource'
+        filename = 'filename'
+        config = {'plugins': {'resource.filename': s.driver_name}}
+        factory = PluginHandlerFactory(config)
+
+        assert_that(calling(factory.get).with_args(resource, filename),
+                    raises(NoSuchHandler))
+
+    def test_given_no_config_when_get_then_raise(self):
+        resource = 'resource'
+        filename = 'filename'
+        config = {'plugins': {}}
+        factory = PluginHandlerFactory(config)
+
+        assert_that(calling(factory.get).with_args(resource, filename),
+                    raises(NoSuchHandler))
+
+    @patch('xivo_confgen.handler.driver')
+    def test_given_driver_found_when_get_then_return_handler(self, stevedore_driver):
+        handler = stevedore_driver.DriverManager.return_value.driver.generate = Mock()
+        resource = 'resource'
+        filename = 'filename'
+        config = {'plugins': {'resource.filename': s.driver_name}}
+        factory = PluginHandlerFactory(config)
+
+        result = factory.get(resource, filename)
+
+        assert_that(result, equal_to(handler))
+
+
+class TestFrontendHandlerFactory(TestCase):
+
+    def test_given_no_frontend_found_when_get_then_raise(self):
+        resource = 'resource'
+        filename = 'filename'
+        frontends = {}
+        factory = FrontendHandlerFactory(frontends)
+
+        assert_that(calling(factory.get).with_args(resource, filename),
+                    raises(NoSuchHandler))
+
+    def test_given_frontend_has_no_callback_when_get_then_raise(self):
+        resource = 'resource'
+        filename = 'filename'
+        frontend = Mock()
+        del frontend.filename
+        frontends = {'resource': frontend}
+        factory = FrontendHandlerFactory(frontends)
+
+        assert_that(calling(factory.get).with_args(resource, filename),
+                    raises(NoSuchHandler))
+
+    def test_given_frontend_found_when_get_then_return_frontend(self):
+        resource = 'resource'
+        filename = 'filename'
+        frontend = Mock()
+        frontends = {'resource': frontend}
+        factory = FrontendHandlerFactory(frontends)
+
+        result = factory.get(resource, filename)
+
+        assert_that(result, equal_to(frontend.filename))
+
+
+class TestNullHandlerFactory(TestCase):
+
+    def test_when_get_then_return_null_handler(self):
+        factory = NullHandlerFactory()
+
+        result = factory.get(s.resource, s.filename)
+
+        assert_that(result(), none())

2d769a389b5e97ac22f3b174f6587b656e3b0b49
Pascal Cadotte Michaud <pcm@wazo.io>
2018-04-25 13:18:42 -0400

    dird_sources: add auth fields for the xivo source type

--- xivo_confgen/plugins/dird_sources.py
+++ xivo_confgen/plugins/dird_sources.py
@@ -92,8 +92,29 @@ class _NoContextSeparationSourceGenerator(object):
         }
 
     def _format_xivo_source(self, source):
-        return {'unique_column': 'id',
-                'confd_config': self._format_confd_config(source)}
+        return {
+            'unique_column': 'id',
+            'confd': self._format_confd_config(source),
+            'auth': self._format_auth_config(source),
+        }
+
+    def _format_auth_config(self, source):
+        config = {
+            'host': source['auth_host'],
+            'port': source['auth_port'],
+            'backend': source['auth_backend'],
+            'username': source['xivo_username'],
+            'password': source['xivo_password'],
+        }
+        if not source['auth_verify_certificate']:
+            verify = False
+        elif source['auth_custom_ca_path']:
+            verify = source['auth_custom_ca_path']
+        else:
+            verify = True
+
+        config['verify_certificate'] = verify
+        return config
 
     def _format_confd_config(self, source):
         scheme, _, end = source['uri'].partition('://')
@@ -109,9 +130,6 @@ class _NoContextSeparationSourceGenerator(object):
         }
         if port:
             config['port'] = int(port)
-        if source['xivo_username'] and source['xivo_password']:
-            config['username'] = source['xivo_username']
-            config['password'] = source['xivo_password']
 
         return config
 

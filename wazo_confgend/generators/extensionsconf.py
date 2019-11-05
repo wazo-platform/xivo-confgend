1add7d98b57be22f680e9b9ae4580a113c059071
Mehdi Abaakouk <mabaakouk@wazo.io>
2019-11-05 14:46:59 +0100

    Use python configparser
    The python configparser is now ordered by default, no need to
    use the backported hacked python 2.4 module.
    
    See: https://www.python.org/dev/peps/pep-0372/

--- wazo_confgend/generators/extensionsconf.py
+++ wazo_confgend/generators/extensionsconf.py
@@ -2,9 +2,10 @@
 # Copyright 2011-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import ConfigParser
 from StringIO import StringIO
 
-from xivo import OrderedConf, xivo_helpers
+from xivo import xivo_helpers
 from xivo_dao import asterisk_conf_dao
 from xivo_dao.resources.ivr import dao as ivr_dao
 
@@ -107,8 +108,10 @@ class ExtensionsConf(object):
 
         if self.contextsconf is not None:
             # load context templates
-            conf = OrderedConf.OrderedRawConf(filename=self.contextsconf)
-            if conf.has_conflicting_section_names():
+            conf = ConfigParser.RawConfigParser()
+            try:
+                conf.read([self.contextsconf])
+            except ConfigParser.DuplicateSectionError:
                 raise ValueError("%s has conflicting section names" % self.contextsconf)
             if not conf.has_section('template'):
                 raise ValueError("Template section doesn't exist in %s" % self.contextsconf)

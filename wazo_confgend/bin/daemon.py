5d74bf97b50972c660217875da30c60adf233adc
Francois Blackburn <fblackburn@wazo.community>
2020-05-07 12:55:27 -0400

    run always in foreground

--- wazo_confgend/bin/daemon.py
+++ wazo_confgend/bin/daemon.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 Avencall
+# Copyright 2016-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import xivo_dao
@@ -11,12 +11,13 @@ from xivo import xivo_logging
 from wazo_confgend.confgen import ConfgendFactory
 from wazo_confgend.config import load as load_config
 
+FOREGROUND = True  # Always in foreground systemd takes care of daemonizing
+
 
 def main():
     config = load_config()
 
-    foreground = True
-    xivo_logging.setup_logging(config['log_filename'], foreground, config['debug'], config['log_level'])
+    xivo_logging.setup_logging(config['log_filename'], FOREGROUND, config['debug'], config['log_level'])
 
     xivo_dao.init_db(config['db_uri'])
     f = ConfgendFactory(config['cache'], config)
@@ -28,8 +29,7 @@ def main():
 def twisted_application():
     config = load_config()
 
-    foreground = False
-    xivo_logging.setup_logging(config['log_filename'], foreground, config['debug'], config['log_level'])
+    xivo_logging.setup_logging(config['log_filename'], FOREGROUND, config['debug'], config['log_level'])
 
     xivo_dao.init_db(config['db_uri'])
     f = ConfgendFactory(config['cache'], config)

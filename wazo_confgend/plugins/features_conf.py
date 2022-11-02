2e9fb0f76a92775e419ca9d3639080f05b4374a9
Charles Langlois <contact@charleslanglois.dev>
2022-11-02 10:48:21 -0400

    wazo_confgend.plugins.features_conf: refactored FeaturesConfGenerator instance logger into module logger for consistency

--- wazo_confgend/plugins/features_conf.py
+++ wazo_confgend/plugins/features_conf.py
@@ -10,10 +10,11 @@ import logging
 import io
 
 
+LOGGER = logging.getLogger(__name__)
+
 class FeaturesConfGenerator:
     def __init__(self, dependencies):
         self._settings = asterisk_conf_dao.find_features_settings()
-        self.logger = logging.getLogger(__name__).getChild(self.__class__.__name__)
 
     def _generate_general(self, ast_file):
         ast_file.write_section('general')
@@ -28,13 +29,13 @@ class FeaturesConfGenerator:
         ast_file.write_options(self._settings['applicationmap_options'])
 
     def generate(self):
-        self.logger.debug("Generating config for features.conf")
+        LOGGER.debug("Generating config for features.conf")
         output = io.StringIO()
         ast_file = AsteriskFileWriter(output)
         self._generate_general(ast_file)
-        self.logger.debug("Generated general section for features.conf")
+        LOGGER.debug("Generated general section for features.conf")
         self._generate_featuremap(ast_file)
-        self.logger.debug("Generated featuremap section for features.conf")
+        LOGGER.debug("Generated featuremap section for features.conf")
         self._generate_applicationmap(ast_file)
-        self.logger.debug("Generated applicationmap section for features.conf")
+        LOGGER.debug("Generated applicationmap section for features.conf")
         return output.getvalue()

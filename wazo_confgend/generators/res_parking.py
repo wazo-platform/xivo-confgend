7fdd3986bfeb0b81abc858d0c912ecb52556dc67
Sebastien Duthil <sduthil@wazo.community>
2024-01-19 11:44:30 +0100

    parking: remove default parking + global options
    Why:
    
    * replaced with multi-tenant parking lots

--- wazo_confgend/generators/res_parking.py
+++ wazo_confgend/generators/res_parking.py
@@ -1,8 +1,7 @@
-# Copyright 2015-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
-from xivo_dao import asterisk_conf_dao
 from xivo_dao.resources.parking_lot import dao as parking_lot_dao
 
 from wazo_confgend.generators.util import AsteriskFileWriter
@@ -10,23 +9,11 @@ from wazo_confgend.generators.util import AsteriskFileWriter
 
 class ResParkingConf:
     def __init__(self):
-        self._settings = asterisk_conf_dao.find_parking_settings()
         self._parking_lots = parking_lot_dao.find_all_by()
 
     def generate(self, output):
         ast_file = AsteriskFileWriter(output)
-        self._generate_general(ast_file)
-        self._generate_default_parking_lot(ast_file)
         self._generate_parking_lots(ast_file)
-
-    def _generate_general(self, ast_file):
-        ast_file.write_section('general')
-        ast_file.write_options(self._settings['general_options'])
-
-    def _generate_default_parking_lot(self, ast_file):
-        for parking_lot in self._settings['parking_lots']:
-            ast_file.write_section(parking_lot['name'])
-            ast_file.write_options(parking_lot['options'])
 
     def _generate_parking_lots(self, ast_file):
         for parking_lot in self._parking_lots:
